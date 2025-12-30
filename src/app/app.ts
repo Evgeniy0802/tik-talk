6cf1a47733d8ac320d73e4b2369858595296b1e2
Evgeniy0808 <zhene4ka0808@gmail.com>
2025-12-31 01:02:35 +0300

    first

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {ProfileCardComponent} from "./common-ui/profile-card/profile-card.component";
+import {ProfileService} from "./data/services/profile.service";
+import {JsonPipe} from "@angular/common";
+import {Profile} from "./data/interfaces/profile.interface";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, ProfileCardComponent],
+    templateUrl: './app.html',
+    styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('tik-talk');
+    profileService = inject(ProfileService);
+    profiles: Profile[] = [] //массив наших профилей, делаем тип данных Profile[], которые создали в интерфейсах и передали в сервисе(getTestAccounts) тип данных
+
+    constructor() {
+        this.profileService.getTestAccounts()
+            .subscribe(val => { //подписался на объект
+                this.profiles = val //вовзвращает некоторое значение, которому мы присваиваем переменной profiles это значение
+            })
+    }
 }
