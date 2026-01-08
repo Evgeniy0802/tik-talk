d741c2e76146352e44306a4f8e2a6cde400ec8b5
Evgeniy0808 <zhene4ka0808@gmail.com>
2026-01-08 19:57:38 +0300

    authorization-login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
-import {Component, inject}                                       from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {AuthService}                                             from "../../auth/auth.service";
+import {AuthService}                       from "../../auth/auth.service";
+import {delay, from, map, skip, take, tap} from "rxjs";
+import {Router}                                                  from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -12,6 +14,9 @@ import {AuthService}                                             from "../../aut
 })
 export class LoginPageComponent {
   authService = inject(AuthService)
+  router = inject(Router)
+
+  isPasswordVisible = signal<boolean>(false) //контейнер для значения, который имеет тип boolean, превостепено он будет false
 
   form = new FormGroup({
     username: new FormControl(null, Validators.required),
@@ -25,8 +30,26 @@ export class LoginPageComponent {
       //@ts-ignore
       this.authService.login(this.form.value)
           .subscribe( res => {
+            this.router.navigate(['']) //делаем навигаицю на пустую строку, на корень проекта
             console.log(res)
           })
     }
+  }
+
+  constructor() {
+    let a = 0
+    from([1,2,3,4,5,6,7,8,9])
+        .pipe(
+            map(val => val * 2), //мэпит так же как в массивах, он что то изменяет
+            tap((val) => { //он берет значение, ты можешь с ним что то сделать, но он никак на конвеер не влияет
+              a = val                 //можем получить значение и что то с ним сделать, обычно сохранить во внешнюю переменную
+                                    //можем написать функцию, которая может поработать с каждым конкретным значением, которое пролетает мимо
+
+            })
+
+        )
+        .subscribe(val => {
+          console.log('subscribe', val)
+        })
   }
 }
