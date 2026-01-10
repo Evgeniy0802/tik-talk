d831db62bcc9ab9e6748c0fe5948a17e2d147258
Evgeniy0808 <zhene4ka0808@gmail.com>
2026-01-10 22:00:18 +0300

    sidebar

---
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'svg[icon]',
+  //все svg у который будет указываться атрибут icon, станут этим компонентом
+  imports: [],
+  template: '<svg:use [attr.href]="href"></svg:use>',
+  styles: [''],
+  //будем байдить некий арибут href, байдим атрибут к переменной
+  //наша переменная будет гетером.
+  //гетером она будет для того что-бы на лету вычисляться свободно
+})
+export class SvgIconComponent {
+  @Input() icon = ''
+
+  get href() {
+    return `/assets/svg/${this.icon}.svg#${this.icon}`;
+    //для этого синитаксиса нужно внутри файла svg сослаться на определённый символ с id
+  }
+}
